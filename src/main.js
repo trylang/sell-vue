0988df1182988ef08da4e3e1069d8c68e59b6755
Jane <trylang@foxmail.com>
2017-08-28 18:22:03 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -2,14 +2,45 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
+import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
+import goods from './components/goods/goods.vue'
+import ratings from './components/ratings/ratings.vue'
+import seller from './components/seller/seller.vue'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+import './common/stylus/index.styl'
+
+// let app = Vue.extend(App)
+
+const routes = [{
+  path: '/goods',
+  component: goods
+}, {
+  path: '/ratings',
+  component: ratings
+}, {
+  path: '/seller',
+  component: seller
+}]
+
+// 3. 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
+const router = new VueRouter({
+  linkActiveClass: 'active',
+  routes // （缩写）相当于 routes: routes，//linkActiveClass：全局设置样式
 })
+
+Vue.use(VueRouter)
+
+// Vue.config.productionTip = false
+
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
+
+// router.start(app, '#app')
